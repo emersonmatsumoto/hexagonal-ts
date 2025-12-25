20f46cd56e7ecad4f886194b5e4c6ec2c6f71f3a
Emerson Katsumi Matsumoto <emerson.matsumoto@outlook.com>
2025-12-25 15:44:27 -0300

    refactor project to join domain and application

--- packages/application/src/index.ts
+++ packages/application/src/index.ts
@@ -1,22 +1,3 @@
-import { Board } from "@kanban/domain";
-import { BoardService } from "./board-service";
-
-const board = new Board("1", "Meu Kanban");
-const service = new BoardService(board);
-
-const todo = service.addColumn("To Do");
-const doing = service.addColumn("Doing");
-
-const card = service.addCard(todo.id, "Estudar TypeScript");
-
-console.log(board.getColumns().map(c => ({
-  name: c.name,
-  cards: c.getCards().map(card => card.title)
-})));
-
-service.moveCard(card.id, todo.id, doing.id);
-
-console.log(board.getColumns().map(c => ({
-  name: c.name,
-  cards: c.getCards().map(card => card.title)
-})));
+export * from "./domain";
+export * from "./port";
+export * from "./service";
---
+++ packages/application/src/service/board-service.ts
@@ -0,0 +1,29 @@
+import { Board, Column, Card } from "../domain";
+import { BoardRepository } from "../port/board-repository";
+
+export class BoardService {
+  constructor(private repo: BoardRepository) {}
+
+  async createBoard(id: string, name: string): Promise<Board> {
+    const board = new Board(id, name);
+    await this.repo.save(board);
+    return board;
+  }
+
+  async addColumn(board: Board, name: string): Promise<Column> {
+    const column = new Column(crypto.randomUUID(), name);
+    board.addColumn(column);
+    await this.repo.save(board);
+    return column;
+  }
+
+  async addCard(board: Board, columnId: string, title: string, description?: string): Promise<Card> {
+    const column = board.getColumns().find(c => c.id === columnId);
+    if (!column) throw new Error("Column not found");
+
+    const card = new Card(crypto.randomUUID(), title, description);
+    column.addCard(card);
+    await this.repo.save(board);
+    return card;
+  }
+}
